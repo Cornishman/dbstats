8d2aebdbea6ce431a5c5b888ed01519036dd9182
Mike <mike.jr.watts@gmail.com>
2014-03-18 23:17:11 +0000

    Initial work on a fix for items like Tinkers Construct hammer, whereby a tool used by a player breaks multiple blocks (currently the system only acknowledges the first block the "player" broke, not the rest)

--- dbStats/EventTrackers/DbBlockBreakEvent.java
+++ dbStats/EventTrackers/DbBlockBreakEvent.java
@@ -1,5 +1,6 @@
 package dbStats.EventTrackers;
 
+import dbStats.Util.ErrorUtil;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.EventPriority;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -9,12 +10,27 @@ import dbStats.API.Statistics.EStatistic;
 import dbStats.Statistics.BlockItemStatistic;
 import dbStats.Statistics.PlayerStatistic;
 import dbStats.Util.Utilities;
+import net.minecraftforge.event.world.BlockEvent;
 
 public class DbBlockBreakEvent {
+
+    @ForgeSubscribe(priority = EventPriority.LOWEST)
+    public void onPlayerHarvestBlock(BlockEvent.HarvestDropsEvent event)
+    {
+        //TODO : Add new priority system to EStatistic that will ignore if the stat matches a recent stat but with a lower priority
+        // this would allow for the multiple break events to be triggered by breaking a block but to only acknowledge 1
+        // Better yet, compare the block x/y/z/ coords against each other, if they match, take the highest priority!
+        if (event.harvester != null && event.block != null)
+        {
+            ErrorUtil.LogMessage(event.harvester.getEntityName() + " broke block " + event.block.blockID + ":" + event.blockMetadata);
+        }
+    }
 
 	@ForgeSubscribe(priority = EventPriority.LOWEST)
 	public void onPlayerBlockBreak(PlayerBlockBreak event)
 	{
+        if (event.player != null) { ErrorUtil.LogMessage(event.player.getEntityName()); }
+
 		if (!event.isCanceled() && Utilities.CanTrackPlayer(event.player))
 		{
 			MinecraftForge.EVENT_BUS.post(new EStatistic(new PlayerStatistic("players", "BlocksBroken", event.player.username, 1, true)));
