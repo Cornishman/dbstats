69b5d4098a285a55f1c5d9652c2776aef6340028
Mike <mike.jr.watts@gmail.com>
2014-03-23 19:16:16 +0000

    changes to block place code

--- dbStats/EventTrackers/DbBlockPlaceEvent.java
+++ dbStats/EventTrackers/DbBlockPlaceEvent.java
@@ -4,9 +4,13 @@ import dbStats.API.Events.PlayerBlockPlace;
 import dbStats.API.Statistics.EStatistic;
 import dbStats.Statistics.BlockItemStatistic;
 import dbStats.Statistics.PlayerStatistic;
+import dbStats.Util.ErrorUtil;
 import dbStats.Util.Utilities;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBed;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemBlockWithMetadata;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.EventPriority;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -18,18 +22,18 @@ public class DbBlockPlaceEvent {
 	{
 		if (!event.isCanceled() && Utilities.CanTrackPlayer(event.player) && event.itemStack != null)
 		{
-            if (event.itemStack.getItem() instanceof ItemBlock || event.itemStack.getItem() instanceof ItemBlockWithMetadata)
-            {
-//                ErrorUtil.LogMessage("This item is a block!");
-
-                MinecraftForge.EVENT_BUS.post(new EStatistic(new PlayerStatistic(0, 0, "players", "BlocksPlaced", event.player.username, 1, true)));
-
                 int blockId = event.itemStack.itemID;
                 int blockMeta = Utilities.GetItemMetaDataValue(event.itemStack);
                 String nbt = Utilities.GetItemNBT(event.itemStack);
-
-                MinecraftForge.EVENT_BUS.post(new EStatistic(new BlockItemStatistic(0, 0, "bistats", "total", event.player.username, blockId, blockMeta, 1, nbt, "place")));
-            }
+                try {
+                    if (Block.blocksList[blockId] != null)
+                    {
+                        MinecraftForge.EVENT_BUS.post(new EStatistic(new PlayerStatistic(0, 0, "players", "BlocksPlaced", event.player.username, 1, true)));
+                        MinecraftForge.EVENT_BUS.post(new EStatistic(new BlockItemStatistic(0, 0, "bistats", "total", event.player.username, blockId, blockMeta, 1, nbt, "place")));
+                    }
+                } catch (Exception ex){
+                    //Not a block so assume item place (ie, wrench, mana bean .. etc)
+                }
 		}
 	}
 }
